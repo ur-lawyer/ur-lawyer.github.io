73818481983dfab394d8c204688228a5acdf8575
Sushil Mohan <codingrhodes@gmail.com>
2025-12-18 06:32:18 +0530

    Added New ssh

--- scripts/generate_posts.py
+++ scripts/generate_posts.py
@@ -55,13 +55,13 @@ write an SEO-optimised blog on the title {title}. using the Focus keyword {focus
 use the following
 
 Rules:
-- Simple English
-- Max 3 sentences per paragraph
+- Simple English, a 10 year old can understand
+- Don't write more than 3 sentences per paragraph, changes paragraph after 3 sentences
 - Use "you" to address the reader
 - Include practical examples related to {focus_kw}
 - Use H2 and H3, h4, h5, h6 headings, no H1
 - Use lists, tables, snippets, and other data formats
-- Write more than 2000 words
+- Write more than 1500 words
 - Write in Jekyll markdown format
 - Naturally include focused & semantic keywords
 - use author: Mary
